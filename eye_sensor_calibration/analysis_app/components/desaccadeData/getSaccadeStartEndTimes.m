b757ecabb0cc77937200ac0c6c74136ac3351297
Brian Angeles <brianangeles@berkeley.edu>
2025-09-18 15:45:50 -0700

    - Fixed issue in `getSaccadeStartEndTimes.m` that allowed an index value of 0, causing errors in the eye calibration analysis app.

--- eye_sensor_calibration/analysis_app/components/desaccadeData/getSaccadeStartEndTimes.m
+++ eye_sensor_calibration/analysis_app/components/desaccadeData/getSaccadeStartEndTimes.m
@@ -9,7 +9,7 @@ goodStarts = find(sacEndPoints == 1);
 goodStops = find(sacEndPoints == -1);
 
 if goodStarts(1) > goodStops(1)
-    goodStarts = [0; goodStarts];
+    goodStarts = [1; goodStarts];
 elseif goodStarts(end) > goodStops(end)
     goodStops = [goodStops; length(~saccades)];
 elseif isempty(goodStarts) && isempty(goodStops)
